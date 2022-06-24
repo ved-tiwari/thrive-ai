2862834f842715ecf102b1ea4745f4bdcb144fcc
Ved Tiwari <79772110+ved-tiwari@users.noreply.github.com>
2022-06-24 15:40:28 -0400

    slides

--- static/main.js
+++ static/main.js
@@ -605,7 +605,7 @@ function plusDivs(n) {
 
 function showDivs(n) {
   var i;
-  var x = document.getElementsByClassName("mySlides");
+  var x = document.getElementsByClassName("slides");
   if (n > x.length) {slideIndex = 1}
   if (n < 1) {slideIndex = x.length} ;
   for (i = 0; i < x.length; i++) {
