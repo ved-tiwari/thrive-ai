b1ef13eca6f5a4e5f12d4858dc53627579d417b8
Ved Tiwari <79772110+ved-tiwari@users.noreply.github.com>
2022-06-24 12:47:09 -0400

    git-update

--- ml.py
+++ ml.py
@@ -5,6 +5,15 @@ import random
 
 app = Flask(__name__)
 
+@app.route('/git_update', methods=['POST'])
+def git_update():
+    repo = git.Repo('./orbe')
+    origin = repo.remotes.origin
+    repo.create_head('main',
+                     origin.refs.main).set_tracking_branch(origin.refs.main).checkout()
+    origin.pull()
+    return '', 200
+
 @app.route("/", methods=["POST", "GET"])
 def index():
 	if request.method == "GET":
